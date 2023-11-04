259fff8a59c0ad11b44f1931eeee61d0e2e93a0f
arcturus-prime <seanans135@gmail.com>
2023-11-04 15:43:23 -0500

    more rotors

--- src/math.zig
+++ src/math.zig
@@ -1,6 +1,83 @@
 const std = @import("std");
 
-pub const Vec3f = @Vector(3, f32);
+pub const Vec3 = @Vector(3, f32);
+
+const Rotor3 = @Vector(4, f32);
+const rotor3 = struct {
+    pub fn exp(b: Rotor3) Rotor3 {
+        const mag: f32 = std.math.sqrt(b[3] * b[3] + b[2] * b[2] + b[1] * b[1]);
+
+        const sin: f32 = @sin(mag) / mag;
+        const cos: f32 = @cos(mag);
+
+        const exp: f32 = 0.5 * @exp(b[0]);
+
+        const scalar: f32 = exp * cos;
+        const vector: f32 = exp * sin;
+
+        return .{
+            scalar + scalar,
+            2 * vector * b[1],
+            2 * vector * b[2],
+            2 * vector * b[3],
+        };
+    }
+
+    pub fn geo(a: Rotor3, b: Rotor3) Multi3 {
+        return .{
+            a[0] * b[0] + a[1] * b[1] + a[2] * b[2] + a[3] * b[3],
+            a[1] * b[2] - a[2] * b[1],
+            a[2] * b[3] - a[3] * b[2],
+            a[3] * b[1] - a[1] * b[3],
+        };
+    }
+
+    pub fn conj(a: Rotor3) Rotor3 {
+        return .{ a[0], -a[1], -a[2], -a[3] };
+    }
+
+    pub fn add(a: Rotor3, b: Rotor3) Rotor3 {
+        return .{
+            a[0] + b[0],
+            a[1] + b[1],
+            a[2] + b[2],
+            a[3] + b[3],
+        };
+    }
+
+    pub fn sub(a: Rotor3, b: Rotor3) Rotor3 {
+        return .{
+            a[0] - b[0],
+            a[1] - b[1],
+            a[2] - b[2],
+            a[3] - b[3],
+        };
+    }
+
+    pub fn grade0(a: Rotor3) f32 {
+        return a[0];
+    }
+
+    pub fn grade2(a: Rotor3) Vec3 {
+        return .{ a[1], a[2], a[3] };
+    }
+
+    pub fn scalar(b: Rotor3, a: f32) Rotor3 {
+        var c: Rotor3 = b;
+        c[0] = a;
+
+        return c;
+    }
+
+    pub fn bivec(b: Rotor3, a: Vec3) Rotor3 {
+        var c: Rotor3 = b;
+        c[1] = a[0];
+        c[2] = a[1];
+        c[3] = a[2];
+
+        return c;
+    }
+};
 
 pub const Multi3 = @Vector(8, f32);
 pub const multi3 = struct {
@@ -86,20 +163,20 @@ pub const multi3 = struct {
         };
     }
 
-    pub fn grade0(a: Multi3) f32 {
-        return a[0];
+    pub fn grade0(b: Multi3) f32 {
+        return b[0];
     }
 
-    pub fn grade1(a: Multi3) Vec3f {
-        return .{ a[1], a[2], a[3] };
+    pub fn grade1(b: Multi3) Vec3 {
+        return .{ b[1], b[2], b[3] };
     }
 
-    pub fn grade2(a: Multi3) Vec3f {
-        return .{ a[4], a[5], a[6] };
+    pub fn grade2(b: Multi3) Vec3 {
+        return .{ b[4], b[5], b[6] };
     }
 
-    pub fn grade3(a: Multi3) f32 {
-        return a[7];
+    pub fn grade3(b: Multi3) f32 {
+        return b[7];
     }
 
     pub fn scalar(b: Multi3, a: f32) Multi3 {
@@ -109,7 +186,7 @@ pub const multi3 = struct {
         return c;
     }
 
-    pub fn vec(b: Multi3, a: Vec3f) Multi3 {
+    pub fn vec(b: Multi3, a: Vec3) Multi3 {
         var c: Multi3 = b;
         c[1] = a[0];
         c[2] = a[1];
@@ -118,7 +195,7 @@ pub const multi3 = struct {
         return c;
     }
 
-    pub fn bivec(b: Multi3, a: Vec3f) Multi3 {
+    pub fn bivec(b: Multi3, a: Vec3) Multi3 {
         var c: Multi3 = b;
         c[4] = a[0];
         c[5] = a[1];
@@ -130,6 +207,17 @@ pub const multi3 = struct {
     pub fn trivec(b: Multi3, a: f32) Multi3 {
         var c: Multi3 = b;
         c[7] = a;
+
+        return c;
+    }
+
+    pub fn rotor(b: Multi3, a: Rotor3) Multi3 {
+        var c: Multi3 = b;
+
+        c[0] = a[0];
+        c[4] = a[1];
+        c[5] = a[2];
+        c[6] = a[3];
 
         return c;
     }

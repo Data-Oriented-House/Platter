bef20f6e2bbdf15248b52408793967139bd8171a
arcturus-prime <seanans135@gmail.com>
2023-11-04 10:06:26 -0500

    more overhaul

--- src/main.zig
+++ src/main.zig
@@ -4,13 +4,11 @@ const math = @import("math.zig");
 pub fn main() !void {
     var bivec: math.Vec3f = .{ 0, 0, 1 };
     var rotor = math.rotor.from.bivecang(bivec, std.math.pi / 2.0);
-    std.debug.print("{any} {any}\n", .{ rotor, math.vector.dot4f(rotor, rotor) });
 
     var point: math.Vec3f = .{ 0, 5, 0 };
     std.debug.print("{any}\n", .{point});
 
     point = math.rotor.apply(rotor, point);
-
     std.debug.print("{any} {any}\n", .{ point, math.vector.dot3f(point, point) });
 
     rotor = math.rotor.compose(math.rotor.invert(rotor), rotor);
--- src/math.zig
+++ src/math.zig
@@ -1,106 +1,73 @@
 const std = @import("std");
 
-pub const Vec2f = @Vector(2, f32);
-pub const Vec3f = @Vector(3, f32);
-pub const Vec4f = @Vector(4, f32);
+const Vec3f = @Vector(3, f32);
+const Vec4f = @Vector(4, f32);
 
-pub const vector = struct {
-    pub fn dot2f(lhs: Vec2f, rhs: Vec2f) f32 {
-        return lhs[0] * rhs[0] + lhs[1] * rhs[1];
-    }
+pub const Multi = struct {
+    pub const Multi3 = @Vector(8, f32);
 
-    pub fn dot3f(lhs: Vec3f, rhs: Vec3f) f32 {
-        return lhs[0] * rhs[0] + lhs[1] * rhs[1] + lhs[2] * rhs[2];
-    }
+    pub fn exp(b: Multi3) Multi3 {
+        const ap: f32 = std.math.sqrt((b[3] + b[4]) * (b[3] + b[4]) + (b[2] + b[6]) * (b[2] + b[6]) + (b[1] + b[5]) * (b[1] + b[5]));
+        const an: f32 = std.math.sqrt((b[3] - b[4]) * (b[3] - b[4]) + (b[2] - b[6]) * (b[2] - b[6]) + (b[1] - b[5]) * (b[1] - b[5]));
 
-    pub fn dot4f(lhs: Vec4f, rhs: Vec4f) f32 {
-        return lhs[0] * rhs[0] + lhs[1] * rhs[1] + lhs[2] * rhs[2] + lhs[3] * rhs[3];
-    }
-};
+        const sinan: f32 = std.math.sin(an) / an;
+        const sinap: f32 = std.math.sin(ap) / ap;
 
-pub const rotor = struct {
-    pub const from = struct {
-        pub fn vec(lhs: Vec3f, rhs: Vec3f) Vec4f {
-            var prod: Vec4f = @splat(0);
+        const cosan: f32 = std.math.cos(an);
+        const cosap: f32 = std.math.cos(ap);
 
-            prod[0] = lhs[0] * rhs[1] - rhs[0] - lhs[1];
-            prod[1] = lhs[0] * rhs[2] - rhs[0] - lhs[2];
-            prod[2] = lhs[1] * rhs[2] - rhs[1] - lhs[2];
-            prod[3] = vector.dot3f(lhs, rhs);
+        const expa: f32 = 0.5 * std.math.exp(b[0]);
+        const exphp: f32 = std.math.exp(b[7]);
+        const exphn: f32 = std.math.exp(-b[7]);
 
-            return prod;
-        }
+        const left: f32 = expa * exphp;
+        const right: f32 = expa * exphn;
 
-        pub fn bivecang(b: Vec3f, angle: f32) Vec4f {
-            return bivector.exp(b * @as(Vec3f, @splat(angle / 2)));
-        }
-    };
+        const leftScalar: f32 = left * cosan;
+        const rightScalar: f32 = right * cosap;
 
-    pub fn apply(r: Vec4f, target: Vec3f) Vec3f {
-        var first: Vec3f = @splat(0);
+        const leftVector: f32 = left * sinan;
+        const rightVector: f32 = right * sinap;
 
-        first[0] = r[3] * target[0] + r[0] * target[1] - r[2] * target[2];
-        first[1] = r[3] * target[1] - r[0] * target[0] + r[1] * target[2];
-        first[2] = r[3] * target[2] - r[1] * target[1] + r[2] * target[0];
-
-        const tri: f32 = r[0] * target[2] + r[1] * target[1] + r[2] * target[0];
-        const conj = invert(r);
-
-        var second: Vec3f = @splat(0);
-
-        second[0] = first[0] * conj[3] - first[1] * conj[0] + first[2] * conj[2] - tri * conj[1];
-        second[1] = first[0] * conj[0] + first[1] * conj[3] - first[2] * conj[1] - tri * conj[2];
-        second[2] = first[0] * conj[2] + first[1] * conj[1] + first[2] * conj[3] - tri * conj[0];
-
-        return second * @as(Vec3f, @splat(2));
-    }
-
-    pub fn compose(rotor1: Vec4f, rotor2: Vec4f) Vec4f {
-        var result: Vec4f = @splat(0);
-
-        result[0] = rotor1[3] * rotor2[0] + rotor1[0] * rotor2[3] + rotor1[1] * rotor2[2] - rotor1[2] * rotor2[1];
-        result[1] = rotor1[3] * rotor2[1] - rotor1[0] * rotor2[2] + rotor1[1] * rotor2[3] + rotor1[2] * rotor2[0];
-        result[2] = rotor1[3] * rotor2[2] + rotor1[0] * rotor2[1] - rotor1[1] * rotor2[0] + rotor1[2] * rotor2[3];
-        result[3] = rotor1[3] * rotor2[3] - rotor1[0] * rotor2[0] - rotor1[1] * rotor2[1] - rotor1[2] * rotor2[2];
-
-        return result;
-    }
-
-    pub fn invert(r: Vec4f) Vec4f {
-        var out: Vec4f = .{ -r[0], -r[1], -r[2], r[3] };
-
-        return out;
-    }
-};
-
-pub const bivector = struct {
-    pub fn exp(b: Vec3f) Vec4f {
-        const mag = std.math.sqrt(b[0] * b[0] + b[2] * b[2] + b[1] * b[1]);
-
-        const sin = std.math.sin(mag);
-        const cos = std.math.cos(mag);
-
-        const result: Vec4f = .{
-            -sin * b[0] / mag,
-            -sin * b[1] / mag,
-            -sin * b[2] / mag,
-            cos,
+        return .{
+            leftScalar + rightScalar,
+            leftVector * (b[1] - b[5]) + rightVector * (b[1] + b[5]),
+            leftVector * (b[2] - b[6]) + rightVector * (b[2] + b[6]),
+            leftVector * (b[3] - b[4]) + rightVector * (b[3] + b[4]),
+            leftVector * (b[4] - b[3]) + rightVector * (b[3] + b[4]),
+            leftVector * (b[5] - b[1]) + rightVector * (b[1] + b[5]),
+            leftVector * (b[6] - b[2]) + rightVector * (b[2] + b[6]),
+            leftScalar - rightScalar,
         };
-
-        return result;
     }
-};
 
-pub const transform = struct {
-    position: @Vector(3, f32),
-    rotation: @Vector(4, f32),
+    pub fn geo(a: Multi3, b: Multi3) Multi3 {
+        return .{
+            a[0] * b[0] + a[1] * b[1] + a[2] * b[2] + a[3] * b[3] - a[4] * b[4] - a[5] * b[5] - a[6] * b[6] - a[7] * b[7],
+            a[0] * b[1] + a[1] * b[0] - a[2] * b[4] + a[3] * b[6] + a[4] * b[2] - a[5] * b[7] - a[6] * b[3] - a[7] * b[5],
+            a[0] * b[2] + a[1] * b[4] + a[2] * b[0] - a[3] * b[5] - a[4] * b[1] + a[5] * b[3] - a[6] * b[7] - a[7] * b[6],
+            a[0] * b[3] - a[1] * b[6] + a[2] * b[5] + a[3] * b[0] - a[4] * b[7] - a[5] * b[2] + a[6] * b[1] - a[7] * b[4],
+            a[0] * b[4] + a[1] * b[2] - a[2] * b[1] + a[3] * b[7] + a[4] * b[0] - a[5] * b[6] + a[6] * b[5] + a[7] * b[3],
+            a[0] * b[5] + a[1] * b[7] + a[2] * b[3] - a[3] * b[2] + a[4] * b[6] + a[5] * b[0] - a[6] * b[4] + a[7] * b[1],
+            a[0] * b[6] - a[1] * b[3] + a[2] * b[7] + a[3] * b[1] - a[4] * b[5] + a[5] * b[4] + a[6] * b[0] + a[7] * b[2],
+            a[0] * b[7] + a[1] * b[5] + a[2] * b[6] + a[3] * b[4] + a[4] * b[3] + a[5] * b[1] + a[6] * b[2] + a[7] * b[0],
+        };
+    }
 
-    pub fn compose(a: transform, b: transform) transform {
-        var c: transform = @splat(0);
+    pub fn rotorbiang(a: Vec3f, angle: f32) Multi3 {
+        _ = angle;
+        _ = a;
+    }
 
-        c.position = rotor.apply(a.rotation, b.position);
-        c.rotation = rotor.compose(a.rotation, b.rotation);
+    pub fn vec3(a: Vec3f) Multi3 {
+        return .{ 0, a[0], a[1], a[2], 0, 0, 0, 0 };
+    }
 
-        return c;
+    pub fn bivec3(a: Vec3f) Multi3 {
+        return .{ 0, 0, 0, 0, a[0], a[1], a[2], 0 };
+    }
+
+    pub fn trivec3(a: f32) Multi3 {
+        return .{ 0, 0, 0, 0, 0, 0, 0, a };
     }
 };

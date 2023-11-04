fc5b6eb8140b25a53ad53c1d2f9c947e0e376f0b
arcturus-prime <seanans135@gmail.com>
2023-11-04 10:14:05 -0500

    funni

--- src/math.zig
+++ src/math.zig
@@ -42,6 +42,10 @@ pub const Multi = struct {
     }
 
     pub fn geo(a: Multi3, b: Multi3) Multi3 {
+
+        // Sona did this by hand
+        //
+        // God rest her soul.
         return .{
             a[0] * b[0] + a[1] * b[1] + a[2] * b[2] + a[3] * b[3] - a[4] * b[4] - a[5] * b[5] - a[6] * b[6] - a[7] * b[7],
             a[0] * b[1] + a[1] * b[0] - a[2] * b[4] + a[3] * b[6] + a[4] * b[2] - a[5] * b[7] - a[6] * b[3] - a[7] * b[5],
